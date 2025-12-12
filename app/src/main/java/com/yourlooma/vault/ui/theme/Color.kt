54ba430c4811d0df03049fa03a31083814312c99
alan sinclair <sinclair.alan000@gmail.com git config --global user.name Alan>
2025-12-12 01:13:03 +0000

    Add Android Studio project

---
+++ app/src/main/java/com/yourlooma/vault/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.yourlooma.vault.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Looma palette (dark mode first)
+val LoomaInk      = Color(0xFF0B1218) // page background
+val LoomaSurface  = Color(0xFF111A22) // cards / surfaces
+val LoomaOutline  = Color(0xFF22303B) // borders / dividers
+val LoomaText     = Color(0xFFE6ECEF) // primary text
+val LoomaTextDim  = Color(0xFF96A4AE) // secondary text
+val LoomaAccent   = Color(0xFF43C7D1) // accent teal
+val LoomaAccentDim= Color(0xFF2BA7B1) // pressed/hover
+
